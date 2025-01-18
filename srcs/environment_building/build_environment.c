36c1094d1f38da39ce8f23e6d76e9bf321fcfa6b
Thomas Chobert <tchobert@bess-f2r6s6.clusters.42paris.fr>
2025-01-18 13:41:11 +0100

    ajout de la modification de SHLVL dans build environment

--- srcs/environment_building/build_environment.c
+++ srcs/environment_building/build_environment.c
@@ -12,6 +12,33 @@
 
 #include "minishell.h"
 
+static t_status	update_shell_lvl(t_variable_list *env)
+{
+	t_variable	*SHLVL;
+	unsigned long		shell_level;
+	char				*new_shell_level;
+
+	SHLVL = find_variable_in_environment_from_key("SHLVL", env);
+	if (SHLVL == NULL)
+		return (PROCESS_FAILURE);
+	shell_level = ft_atol(SHLVL->value);
+	shell_level += 1;
+	if (shell_level == 10000)
+	{
+		shell_level = 1;
+		printf("minishell: warning: shell level (10000) too high, "
+		"resetting to 1\n");
+	}
+	new_shell_level = ft_ltoa_base(shell_level, DECIMAL_BASE);
+	update_variable(SHLVL, new_shell_level, EXPORTABLE);
+	return (PROCESS_SUCCESS);
+}
+
+static t_status	update_requested_variables(t_variable_list *env)
+{
+	return (update_shell_lvl(env));
+}
+
 t_status	build_environment(char **variables)
 {
 	size_t	i;
@@ -24,5 +51,5 @@ t_status	build_environment(char **variables)
 			return (PROCESS_FAILURE);
 		++i;
 	}
-	return (PROCESS_SUCCESS);
+	return (update_requested_variables(get_environment()));
 }
