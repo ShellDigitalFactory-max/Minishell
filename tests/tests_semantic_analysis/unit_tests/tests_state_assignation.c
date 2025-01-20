c19a08bc0d8b1d6f050c0b1ae5f0851fbe952861
Thomas Chobert <tchobert@paul-f3Br6s4.clusters.42paris.fr>
2025-01-20 15:31:10 +0100

    state assignation tests ok

--- tests/tests_semantic_analysis/unit_tests/tests_state_assignation.c
+++ tests/tests_semantic_analysis/unit_tests/tests_state_assignation.c
@@ -39,4 +39,30 @@ void	tests_state_assignation(void)
 	TEST_ASSERT_EQUAL(0, ft_strcmp(expected_value, ((t_variable *)command.command_environment->content)->value));
 	printf("Command environment =\n");
 	print_env(&command.command_environment);
+
+	// CLEAR
+
+	delete_variables_list_in_environment(&command.command_environment);
+	ft_bzero(&command, sizeof(t_command));
+	ft_bzero(&token, sizeof(t_token));
+
+	// ARRANGE 2
+
+	token.token_lexem = "THIS IS NOT A VARIABLE";
+	token.token_type = WORD;
+
+	// ACT 2
+
+	expected_return = state_assignation(&machine_state, &token, &command);
+
+	// ASSERT 2
+
+	TEST_ASSERT_EQUAL(machine_state, STATE_INPUT_REDIRECT);
+	TEST_ASSERT_EQUAL(expected_return, TOKEN_NOT_PROCESSED);
+	TEST_ASSERT_EQUAL(NULL, (command.command_environment));
+
+	// CLEAR 2
+
+	ft_bzero(&command, sizeof(t_command));
+	ft_bzero(&token, sizeof(t_token));
 }
