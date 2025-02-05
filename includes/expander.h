b0b5b26d2e09c7a3aa52a25e95dacb6330deb40e
hchereau <hchereau42@gmail.com>
2025-02-05 15:41:50 +0100

    work on quote

--- includes/expander.h
+++ includes/expander.h
@@ -6,12 +6,19 @@
 /*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 03:34:13 by linux             #+#    #+#             */
-/*   Updated: 2025/02/04 06:28:31 by linux            ###   ########.fr       */
+/*   Updated: 2025/02/05 15:13:25 by linux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+typedef enum	e_quote_state
+{
+	NO_QUOTE,
+	SINGLE_QUOTE,
+	DOUBLE_QUOTE,
+} t_quote_state;
 
 t_lexem	expand_tilde(size_t *i, t_lexem expanded_word);
 t_lexem	get_variable_from_env(const char *key);
