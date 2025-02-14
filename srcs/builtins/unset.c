2709e9b4580f4f85c78eefa8d12121eddb374eeb
hchereau <115716283+hchereau@users.noreply.github.com>
2025-02-14 15:38:14 +0100

    Fix/expander (#31)
    * it works
    
    * finish
    
    * Suppression des printf de debug dans no expand
    
    ---------
    
    Co-authored-by: Thomas Chobert <tchobert@paul-f3Br7s1.clusters.42paris.fr>

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:21:06 by tchobert          #+#    #+#             */
-/*   Updated: 2025/02/07 17:21:15 by tchobert         ###   ########.fr       */
+/*   Updated: 2025/02/13 23:10:38 by linux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	unset(t_command *command)
 {
-	t_variable_list	variable_to_delete
+	t_variable_list	variable_to_delete;
 }
