161d6bd3bec04c4052b9912f1b191ff58a3fe9f0
moi <hchereau42@gmail.com>
2025-02-14 17:01:53 +0100

    norm ?

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 03:34:13 by linux             #+#    #+#             */
-/*   Updated: 2025/02/13 14:10:14 by linux            ###   ########.fr       */
+/*   Updated: 2025/02/14 17:00:01 by linux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_lexem	expand_word(t_lexem word, t_quote_state *quote_state);
 t_lexem	no_expand(t_lexem word, size_t *i, t_lexem expanded_word,
 			t_quote_state quote_state);
 t_lexem	expand_variable(t_lexem word, size_t *i, t_lexem expanded_word,
-		t_quote_state quote_state);
+			t_quote_state quote_state);
 bool	is_quote(char c, t_quote_state quote_state);
 void	change_quote_state(char quote, t_quote_state *quote_state);
 void	expander_exit(void);
